bc8c5c2eafd7ddd72f17878d782d20c969d70b5b
danijeljw-rpc <danijel@repasscloud.com>
2023-06-04 09:34:08 +1000

    update homepage

--- wanderconnect-android/Pages/Tabs/HomePage.xaml.cs
+++ wanderconnect-android/Pages/Tabs/HomePage.xaml.cs
@@ -5,5 +5,14 @@ public partial class HomePage : ContentPage
 	public HomePage()
 	{
 		InitializeComponent();
+		Lbl_Username.Text = "Hi, "
+			+ Preferences.Get("username", string.Empty)
+			+ "!";
 	}
+
+    void Cv_Experiences_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
+    {
+		var currentSelection = e.CurrentSelection.FirstOrDefault() as Category;
+		if (currentSelection == null) return;
+    }
 }
