69ad6e7f64956e4498438f919081b95a415b3cc9
danijeljw-rpc <danijel@repasscloud.com>
2023-06-04 00:05:38 +1000

    add routes

--- wanderconnect-android/AppShell.xaml.cs
+++ wanderconnect-android/AppShell.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace wanderconnect_android;
+﻿using wanderconnect_android.Pages;
+
+namespace wanderconnect_android;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		// add routes
+		Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+		Routing.RegisterRoute(nameof(CustomTabbedPage), typeof(CustomTabbedPage));
 	}
 }
 
