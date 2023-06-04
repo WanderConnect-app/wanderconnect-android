f1e411226331019c8d8cbe0c9af9c9285b147d84
danijeljw-rpc <danijel@repasscloud.com>
2023-06-04 22:49:15 +1000

    geolocation added

--- wanderconnect-android/Platforms/Android/MainApplication.cs
+++ wanderconnect-android/Platforms/Android/MainApplication.cs
@@ -1,6 +1,13 @@
 ﻿using Android.App;
 using Android.Runtime;
 
+// geolocation requirements
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesFeature("android.hardware.location", Required = false)]
+[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+[assembly: UsesFeature("android.hardware.location.network", Required = false)]
+
 namespace wanderconnect_android;
 
 [Application]
